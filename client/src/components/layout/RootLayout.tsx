42af96e16c81cacae963af848e84cb30325bd89d
Hsin-tingCHI <chihsinting@gmail.com>
2023-09-06 16:48:16 +0200

    feat: add background image and adjust scroll behavior on mobile and desktop

--- client/src/components/layout/RootLayout.tsx
+++ client/src/components/layout/RootLayout.tsx
@@ -6,9 +6,9 @@ import ButtomMenu from "../ButtomMenu";
 export default function RootLayout() {
   return (
     <div className="lg:grid lg:h-screen lg:grid-cols-12">
-      <SideMenu className="hidden lg:col-span-2 lg:block" />
+      <SideMenu className="hidden dark:bg-gray-900 lg:col-span-2 lg:block" />
       <ButtomMenu className="lg:col-span-2 lg:hidden" />
-      <main className="mb-[60px] lg:col-span-10 lg:mb-0">
+      <main className="mb-[60px] bg-zinc-100 bg-[url('@/assets/layered-waves-haikei-light.svg')] bg-cover bg-fixed dark:bg-background dark:bg-[url('@/assets/layered-waves-haikei.svg')] lg:col-span-10 lg:mb-0 lg:h-screen lg:bg-local">
         <Outlet />
       </main>
     </div>
