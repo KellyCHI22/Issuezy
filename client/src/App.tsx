f45dd1edec318bb4353d7ac43fdff6ac2e8bbc65
Hsin-tingCHI <chihsinting@gmail.com>
2023-09-07 19:17:09 +0200

    feat: add routes for login, signup, tasks and settings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,16 +12,26 @@ import RootLayout from "./components/layout/RootLayout";
 import AllProjectsPage from "./pages/AllProjectsPage";
 import ProjectPage from "./pages/ProjectPage";
 import IssuePage from "./pages/IssuePage";
+import TasksPage from "./pages/TasksPage";
+import AccountPage from "./pages/AccountPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route path="/projects" element={<AllProjectsPage />} />
-      <Route path="/projects/:id" element={<ProjectPage />} />
-      <Route path="/projects/:id/issues/:iid" element={<IssuePage />} />
-    </Route>,
+    <>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/" element={<RootLayout />}>
+        <Route path="/projects" element={<AllProjectsPage />} />
+        <Route path="/projects/:id" element={<ProjectPage />} />
+        <Route path="/projects/:id/issues/:iid" element={<IssuePage />} />
+        <Route path="/tasks" element={<TasksPage />} />
+        <Route path="/account" element={<AccountPage />} />
+      </Route>
+    </>,
   ),
 );
 
