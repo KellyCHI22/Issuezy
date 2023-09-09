cb43058118d9f895eb900263d1a0eae6a72eb622
Hsin-tingCHI <chihsinting@gmail.com>
2023-09-09 22:45:12 +0200

    fix: remove checking user permission on every page change

--- client/src/components/ProtectedRoutes.tsx
+++ client/src/components/ProtectedRoutes.tsx
@@ -1,11 +1,10 @@
 import { checkPermission } from "@/apis/auth-api";
 import { useMutation } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export default function ProtectedRoutes() {
   const navigate = useNavigate();
-  const { pathname } = useLocation();
   const permissionMutation = useMutation({
     mutationFn: checkPermission,
     onSuccess: (data) => console.log(data),
@@ -18,7 +17,7 @@ export default function ProtectedRoutes() {
     const token = localStorage.getItem("token");
     permissionMutation.mutate({ token });
     console.log("use effect runs");
-  }, [pathname]);
+  }, []);
 
   return <Outlet />;
 }
