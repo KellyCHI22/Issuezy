0ec60f3c7547c0539227d21f60830972012f313b
Hsin-tingCHI <chihsinting@gmail.com>
2023-09-13 13:13:04 +0200

    feat: use form in IssueSheet and connect post issue api

--- client/src/components/IssueSheet.tsx
+++ client/src/components/IssueSheet.tsx
@@ -12,7 +12,6 @@ import {
 } from "@/components/ui/select";
 import {
   Sheet,
-  SheetClose,
   SheetContent,
   SheetDescription,
   SheetFooter,
@@ -20,95 +19,218 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { useMediaQuery } from "react-responsive";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { postIssue } from "@/apis/issue-api";
+import { AlertMessage } from "./AlertMassage";
 
-export function IssueSheet() {
+const issueFormSchema = z.object({
+  title: z
+    .string()
+    .min(1, {
+      message: "Title cannot be blank",
+    })
+    .max(60, {
+      message: "Title cannot be more than 60 characters",
+    }),
+  description: z
+    .string()
+    .min(1, {
+      message: "Description cannot be blank",
+    })
+    .max(300, {
+      message: "Description cannot be more than 300 characters",
+    }),
+  priority: z.number().min(1).max(3),
+  categoryId: z.string(),
+});
+
+export function IssueSheet({ project }) {
+  const [open, setOpen] = useState(false);
+  const [addIssueError, setAddIssueError] = useState<string>("");
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+  const queryClient = useQueryClient();
+  const issueMutation = useMutation({
+    mutationFn: postIssue,
+    onSuccess: (data) => {
+      const { newIssue } = data.data;
+      queryClient.setQueryData(
+        ["projects", project.id, "issues", newIssue.id],
+        newIssue,
+      );
+      queryClient.invalidateQueries(
+        ["projects", project.id.toString(), "issues"],
+        {
+          exact: true,
+        },
+      );
+      setOpen(false);
+    },
+    onError: (error) => setAddIssueError(error.response.data.message),
+  });
+
+  const form = useForm<z.infer<typeof issueFormSchema>>({
+    resolver: zodResolver(issueFormSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      priority: 1,
+      categoryId: project.categories[0].id.toString(),
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof issueFormSchema>) {
+    issueMutation.mutate({ projectId: project.id, formData: values });
+  }
+
+  useEffect(() => {
+    if (form.formState.isSubmitSuccessful) {
+      form.reset({
+        title: "",
+        description: "",
+        priority: 1,
+        categoryId: project.categories[0].id.toString(),
+      });
+    }
+  }, [form.formState]);
 
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
-        <Button variant="default">Add Issue</Button>
+        <Button variant="default">Report an issue</Button>
       </SheetTrigger>
       <SheetContent
         side={isMobile ? "bottom" : "right"}
-        className="dark:bg-gray-900"
+        className="overflow-y-scroll dark:bg-gray-900"
       >
-        <SheetHeader>
-          <SheetTitle>Add Issue</SheetTitle>
-          <SheetDescription>
-            Add a new issue here. Click save when you're done.
-          </SheetDescription>
-        </SheetHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="title" className="text-right">
-              Title
-            </Label>
-            <Input
-              id="title"
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+            <SheetHeader>
+              <SheetTitle>Report an issue</SheetTitle>
+              <SheetDescription>
+                Add a new issue here. Click save when you're done.
+              </SheetDescription>
+            </SheetHeader>
+            {addIssueError && (
+              <AlertMessage variant="destructive" message={addIssueError} />
+            )}
+            <FormField
+              control={form.control}
               name="title"
-              value="Pedro Duarte"
-              className="col-span-3"
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center">
+                  <FormLabel className="text-sm font-bold">Title</FormLabel>
+                  <FormControl className="col-span-3">
+                    <Input placeholder="New issue" {...field} />
+                  </FormControl>
+                  <FormMessage className="col-span-4" />
+                </FormItem>
+              )}
             />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="description" className="text-right">
-              Description
-            </Label>
-            <Textarea
-              id="description"
+            <FormField
+              control={form.control}
               name="description"
-              value="some description"
-              rows={4}
-              className="col-span-3"
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center">
+                  <FormLabel className="text-sm font-bold">
+                    Description
+                  </FormLabel>
+                  <FormControl className="col-span-3">
+                    <Textarea
+                      rows={4}
+                      placeholder="Something about this issue..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="col-span-4" />
+                </FormItem>
+              )}
             />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="priority" className="text-right">
-              Priority
-            </Label>
-            <RadioGroup
-              id="priority"
+            <FormField
+              control={form.control}
               name="priority"
-              defaultValue="option-one"
-              className="col-span-3"
-            >
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="option-one" id="option-one" />
-                <Label htmlFor="option-one">High</Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="option-two" id="option-two" />
-                <Label htmlFor="option-two">Medium</Label>
-              </div>
-              <div className="flex items-center space-x-2">
-                <RadioGroupItem value="option-three" id="option-two" />
-                <Label htmlFor="option-three">Low</Label>
-              </div>
-            </RadioGroup>
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="category" className="text-right">
-              Category
-            </Label>
-            <Select name="category">
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Category" />
-              </SelectTrigger>
-              <SelectContent id="category">
-                <SelectItem value="light">Light</SelectItem>
-                <SelectItem value="dark">Dark</SelectItem>
-                <SelectItem value="system">System</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-        </div>
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button type="submit">Save changes</Button>
-          </SheetClose>
-        </SheetFooter>
+              render={({ field }) => (
+                <FormItem className=" flex flex-row items-center justify-between gap-3 rounded-lg border p-4">
+                  <div className="space-y-0.5">
+                    <FormLabel className="text-sm font-bold">
+                      Priority
+                    </FormLabel>
+                    <FormDescription>
+                      Public projects and issues can be viewed by all users.
+                    </FormDescription>
+                  </div>
+                  <FormControl className="col-span-3">
+                    <RadioGroup
+                      id="priority"
+                      name="priority"
+                      defaultValue={field.value.toString()}
+                      className="col-span-3"
+                    >
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="1" id="high" />
+                        <Label htmlFor="high">High</Label>
+                      </div>
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="2" id="medium" />
+                        <Label htmlFor="medium">Medium</Label>
+                      </div>
+                      <div className="flex items-center space-x-2">
+                        <RadioGroupItem value="3" id="low" />
+                        <Label htmlFor="low">Low</Label>
+                      </div>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage className="col-span-4" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="categoryId"
+              render={({ field }) => (
+                <FormItem className="grid grid-cols-4 items-center">
+                  <FormLabel className="text-sm font-bold">Category</FormLabel>
+                  <div className="col-span-3">
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <FormControl>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select category" />
+                        </SelectTrigger>
+                      </FormControl>
+                      <SelectContent>
+                        {project.categories.map((category) => (
+                          <SelectItem value={category.id.toString()}>
+                            {category.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </div>
+                  <FormMessage className="col-span-4" />
+                </FormItem>
+              )}
+            />
+            <SheetFooter>
+              <Button type="submit">Add Issue</Button>
+            </SheetFooter>
+          </form>
+        </Form>
       </SheetContent>
     </Sheet>
   );
