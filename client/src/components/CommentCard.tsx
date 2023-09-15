c58cad9ef51fa3242491f50b934adb684d2790e8
Hsin-tingCHI <chihsinting@gmail.com>
2023-09-15 16:55:16 +0200

    feat: add dropdown actions menu to CommentCard

--- client/src/components/CommentCard.tsx
+++ client/src/components/CommentCard.tsx
@@ -1,29 +1,47 @@
+import { Card, CardContent } from "@/components/ui/card";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { formatTime } from "@/lib/utils";
 import UserAvatar from "./UserAvatar";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "./ui/button";
 
 export default function CommentCard({ comment }) {
   return (
     <Card className="dark:bg-gray-900">
-      <CardHeader className="">
-        <CardDescription className="flex flex-row items-start justify-between">
-          <div className="flex items-center gap-3">
+      <CardContent>
+        <div className="flex flex-row items-center justify-between pt-6 text-sm text-muted-foreground">
+          <div className="flex items-center">
             <UserAvatar user={comment.User} />
-            <span>{comment.User.firstname}</span>
-            <span>{comment.User.lastname}</span>
+            <div className="hidden space-x-1 px-3 lg:block">
+              <span>{comment.User.firstname}</span>
+              <span>{comment.User.lastname}</span>
+            </div>
+            <span className="pl-3 lg:p-0">{formatTime(comment.createdAt)}</span>
           </div>
-          <span>{formatTime(comment.createdAt)}</span>
-        </CardDescription>
-      </CardHeader>
-      <CardContent>{comment.text}</CardContent>
-      <CardFooter></CardFooter>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Edit comment</DropdownMenuItem>
+              <DropdownMenuItem>Delete comment</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <p className="pb-2 pt-4">{comment.text}</p>
+      </CardContent>
     </Card>
   );
 }
